60c2c65eb20811afa23af348b9ee881cbf024290
sekharkafle <skafle@gmail.com>
2024-04-23 08:48:03 -0400

    removed service url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,13 @@ import { ChatWindow } from "@/components/ChatWindow";
 import { IntermediateStep } from "@/components/IntermediateStep";
 
 export default function Home() {
+  const endPoint=""
   const InfoCard = (
     <div ></div>
   );
   return (
     <ChatWindow
-      endpoint="https://f4wo0au9r5.execute-api.us-east-1.amazonaws.com/default/claude-chat"
+      endpoint={endPoint}
       emoji=""
       titleText="Chatty Assistant"
       placeholder="I'm a chatty LLM assistant. Ask me anything!"
